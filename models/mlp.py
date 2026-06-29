36cedbfda008ec8776237bd54be0339a209e411b
Sam Armstrong <samuel_e_armstrong@yahoo.co.uk>
2026-06-29 02:34:53 +0100

    add mlp model

---
+++ models/mlp.py
@@ -0,0 +1,25 @@
+import torch
+import torch.nn as nn
+
+from globals import (
+    INPUT_SIZE,
+    NUM_CLASSES,
+)
+
+
+class MLPModel(nn.Module):
+
+    def __init__(
+        self,
+        input_size: int = INPUT_SIZE,
+        num_classes: int = NUM_CLASSES,
+    ):
+        super().__init__()
+        self.fc1 = nn.Linear(input_size, 128)
+        self.fc2 = nn.Linear(128, 64)
+        self.fc3 = nn.Linear(64, num_classes)
+
+    def forward(self, features: torch.Tensor) -> torch.Tensor:
+        x = torch.relu(self.fc1(features))
+        x = torch.relu(self.fc2(x))
+        return self.fc3(x)
