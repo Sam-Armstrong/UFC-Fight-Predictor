ecbf7461bb89d980a82434ef6d5c8dc919500935
Sam Armstrong <samuel_e_armstrong@yahoo.co.uk>
2026-06-29 01:56:10 +0100

    update globals.py

--- globals.py
+++ globals.py
@@ -5,3 +5,50 @@ TRAINING_DATA_CSV = "data/TrainingData.csv"
 
 MIN_FIGHTS = 3
 VERBOSE = False
+
+FEATURE_COLUMNS = [
+    "Height 1",
+    "Reach 1",
+    "Age 1",
+    "Knockdowns PM 1",
+    "Gets Knocked Down PM 1",
+    "Sig Strikes Landed PM 1",
+    "Sig Strikes Attempted PM 1",
+    "Sig Strikes Absorbed PM 1",
+    "Strikes Landed PM 1",
+    "Strikes Attempted PM 1",
+    "Strikes Absorbed PM 1",
+    "Strike Accuracy 1",
+    "Takedowns PM 1",
+    "Takedown Attempts PM 1",
+    "Gets Taken Down PM 1",
+    "Submission Attempts PM 1",
+    "Clinch Strikes PM 1",
+    "Clinch Strikes Taken PM 1",
+    "Grounds Strikes PM 1",
+    "Ground Strikes Taken PM 1",
+    "Height 2",
+    "Reach 2",
+    "Age 2",
+    "Knockdowns PM 2",
+    "Gets Knocked Down PM 2",
+    "Sig Strikes Landed PM 2",
+    "Sig Strikes Attempted PM 2",
+    "Sig Strikes Absorbed PM 2",
+    "Strikes Landed PM 2",
+    "Strikes Attempted PM 2",
+    "Strikes Absorbed PM 2",
+    "Strike Accuracy 2",
+    "Takedowns PM 2",
+    "Takedown Attempts PM 2",
+    "Gets Taken Down PM 2",
+    "Submission Attempts PM 2",
+    "Clinch Strikes PM 2",
+    "Clinch Strikes Taken PM 2",
+    "Grounds Strikes PM 2",
+    "Ground Strikes Taken PM 2",
+]
+LABEL_COLUMNS = ["Win", "Loss", "Draw"]
+OUTCOME_NAMES = ["fighter 1 win", "fighter 1 loss", "draw"]
+INPUT_SIZE = len(FEATURE_COLUMNS)
+NUM_CLASSES = len(LABEL_COLUMNS)
